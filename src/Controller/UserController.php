49876905e458f8ba92058f62f35af682d6073f87
tony <tony.marques@live.fr>
2023-09-17 12:21:50 +0200

    add login and logout

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,13 +3,14 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\RegisterType;
 use DateTimeImmutable;
+use App\Form\RegisterType;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserController extends AbstractController
@@ -17,9 +18,9 @@ class UserController extends AbstractController
     #[Route(path: '/inscription', name: 'app.register')]
     public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
     {
-        if ($this->getUser()) {
-            return $this->redirectToRoute("app.home");
-        }
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute("app.home");
+        // }
 
         $user = new User();
         $form = $this->createForm(RegisterType::class, $user);
@@ -32,11 +33,32 @@ class UserController extends AbstractController
             $em->persist($user);
             $em->flush();
 
-            // return $this->redirectToRoute("app.login");
+            return $this->redirectToRoute("app.login");
         }
 
         return $this->render('user/register.html.twig', [
             "form" => $form->createView()
         ]);
+    }
+
+    #[Route('/connexion', name: 'app.login')]
+    public function connexion(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //    return $this->redirectToRoute("app.home");
+        // }
+
+        $error = $authenticationUtils->getLastAuthenticationError();
+        $username = $authenticationUtils->getLastUsername();
+
+        return $this->render("user/login.html.twig", [
+            "error" => $error,
+            "username" => $username
+        ]);
+    }
+
+    #[Route(path: "/deconnexion", name: "app.logout")]
+    public function logout()
+    {
     }
 }
