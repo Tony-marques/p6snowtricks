a93daf62ef9ba14859433e3ff47a9e6e392e06a6
tony <tony.marques@live.fr>
2023-09-18 14:22:39 +0200

    create tricks card on home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\TrickRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,9 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app.home')]
-    public function index(): Response
+    public function index(TrickRepository $trickRepository): Response
     {
-        return $this->render(view:'home/index.html.twig');
+        $tricks = $trickRepository->findAll();
+
+        return $this->render('home/index.html.twig', [
+            "tricks" => $tricks
+        ]);
     }
 
 }
