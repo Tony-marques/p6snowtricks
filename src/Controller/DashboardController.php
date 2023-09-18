c4384b6f2e62fdc411a1660bb037045a8b548de6
tony <tony.marques@live.fr>
2023-09-18 16:58:05 +0200

    add restriction page on dashboard

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,12 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted("ROLE_ADMIN")]
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app.dashboard')]
     public function index(): Response
     {
+//        dd($this->GetUser());
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
